21cf0391b5a622d1511ef1ab22d28d10c0ad03c1
Hasan Taghiyev <hasantaghiev77@gmail.com>
2024-01-17 00:01:48 +0400

    Port moved to .env

--- src/app.js
+++ src/app.js
@@ -25,6 +25,8 @@ app.use(auth);
 app.use(logger);
 app.use('/event', eventRoutes);
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
